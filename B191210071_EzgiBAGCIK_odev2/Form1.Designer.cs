a396e5f8f2ea87bd48d033b67b114276cda92338
ezgibagcik <ezgibagcik@hotmail.com>
2021-05-31 14:52:02 +0300

    first commit

---
+++ B191210071_EzgiBAGCIK_odev2/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace B191210071_EzgiBAGCIK_odev2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblX = new System.Windows.Forms.Label();
+            this.lblY = new System.Windows.Forms.Label();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.btnArkadas = new System.Windows.Forms.Button();
+            this.btnSon = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblX
+            // 
+            this.lblX.AutoSize = true;
+            this.lblX.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblX.Location = new System.Drawing.Point(37, 51);
+            this.lblX.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(18, 17);
+            this.lblX.TabIndex = 0;
+            this.lblX.Text = "X";
+            // 
+            // lblY
+            // 
+            this.lblY.AutoSize = true;
+            this.lblY.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblY.Location = new System.Drawing.Point(37, 96);
+            this.lblY.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblY.Name = "lblY";
+            this.lblY.Size = new System.Drawing.Size(18, 17);
+            this.lblY.TabIndex = 1;
+            this.lblY.Text = "Y";
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(63, 46);
+            this.txtX.Margin = new System.Windows.Forms.Padding(4);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(132, 22);
+            this.txtX.TabIndex = 2;
+            this.txtX.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtX_KeyPress);
+            this.txtX.Validated += new System.EventHandler(this.txtX_Validated);
+            // 
+            // txtY
+            // 
+            this.txtY.Location = new System.Drawing.Point(63, 96);
+            this.txtY.Margin = new System.Windows.Forms.Padding(4);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(132, 22);
+            this.txtY.TabIndex = 3;
+            this.txtY.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtY_KeyPress);
+            this.txtY.Validated += new System.EventHandler(this.txtY_Validated);
+            // 
+            // btnArkadas
+            // 
+            this.btnArkadas.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnArkadas.Location = new System.Drawing.Point(35, 181);
+            this.btnArkadas.Margin = new System.Windows.Forms.Padding(4);
+            this.btnArkadas.Name = "btnArkadas";
+            this.btnArkadas.Size = new System.Drawing.Size(151, 42);
+            this.btnArkadas.TabIndex = 4;
+            this.btnArkadas.Text = "ARKADAŞ MI ?";
+            this.btnArkadas.UseVisualStyleBackColor = true;
+            this.btnArkadas.Click += new System.EventHandler(this.btnArkadas_Click);
+            // 
+            // btnSon
+            // 
+            this.btnSon.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSon.Location = new System.Drawing.Point(220, 181);
+            this.btnSon.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSon.Name = "btnSon";
+            this.btnSon.Size = new System.Drawing.Size(151, 42);
+            this.btnSon.TabIndex = 5;
+            this.btnSon.Text = "SON";
+            this.btnSon.UseVisualStyleBackColor = true;
+            this.btnSon.Click += new System.EventHandler(this.btnSon_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 249);
+            this.Controls.Add(this.btnSon);
+            this.Controls.Add(this.btnArkadas);
+            this.Controls.Add(this.txtY);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.lblY);
+            this.Controls.Add(this.lblX);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Arkadaş Sayılar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblX;
+        private System.Windows.Forms.Label lblY;
+        private System.Windows.Forms.Button btnArkadas;
+        private System.Windows.Forms.Button btnSon;
+        public System.Windows.Forms.TextBox txtX;
+        public System.Windows.Forms.TextBox txtY;
+    }
+}
+
---
+++ B191210071_EzgiBAGCIK_odev2/Form1.cs
@@ -0,0 +1,242 @@
+﻿/**********************************************************************************************************************************************************
+**                                       SAKARYA UNIVERSITESI BILGİISAYAR VE BILIŞIM BILIMLERI FAKULTESI
+**				                                           BILGISAYAR MUHENDISLIĞI BOLUMU
+**				                                           NESNEYE DAYALI PROGRAMLAMA DERSI
+**					                                           2019-2020 BAHAR DÖNEMİ
+* 
+**                                                ÖDEV NUMARASI............: 2.ODEV
+**                                                ÖĞRENCİ ADI..............: EZGI BAĞCIK
+**                                                ÖĞRENCİ NUMARASI.........: B191210071
+**                                                DERS GRUBU...............: 1. OĞRETIM C GRUBU
+***********************************************************************************************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace B191210071_EzgiBAGCIK_odev2
+{
+    public partial class Form1 : Form
+    {
+        //Veriler girildikten sonra arkadas mi butonuna basildiginda cıkacak aracları olusturur
+        ListBox lstBox = new ListBox();
+        ListBox lstBox2 = new ListBox();
+        Label lblX2 = new Label();
+        Label lblY2 = new Label();
+        Label lblToplamlar = new Label();
+        TextBox txtToplamXinAlti = new TextBox();
+        TextBox txtToplamYinAlti = new TextBox();
+        
+        private bool state = false; //Textboxların dolulugunu kontrol etmek icin tanimlanmistir.
+        private int sayi1;
+        private int sayi2;
+        private int toplamX = 0;
+        private int toplamY = 0;
+
+        ErrorProvider provider = new ErrorProvider(); //Bos gecildiginde uyari vermeyi saglamak icin olusturulan nesne.
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnArkadas_Click(object sender, EventArgs e)
+        {
+            this.AutoSize = true; //form ekraninin boyutlarini otomatik ayarlanir.
+            ArkadasMı(); //sayilari carpanlarina ayirip carpanlarini toplayan fonksiyon cagirilir
+            if (state == false) //ArkadasMı() fonksiyonu calistiktan sonra arkadas olup olmadigini kontrol etmek icin kullanilan kosuldur.
+            {
+                if (txtToplamXinAlti.Text == txtY.Text && txtToplamYinAlti.Text == txtX.Text)  //arkadassalar ekranı olusturmasını saglayan kosuldur
+                {
+                    EkranOlustur(); //arkadas mi butonuna bastiktan sonraki ekrani olusturacak fonksiyon cagirilir.
+                }
+                else //arkadas degilseler ekrana arkadas degiller uyarisini cikartan kosuldur.
+                {
+                    MessageBox.Show(txtX.Text+" ve "+txtY.Text+" arkadaş sayı değiller.", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    btnSon_Click(null,null); //arkadas degilseler uyarı ekranı cikarildiktan sonra "btnson_click" olayında olacaklar yapilir.
+                }
+            }
+          
+        }
+
+        private void btnSon_Click(object sender, EventArgs e)
+        {
+            Temizle(); //son butonuna basildiginda ekrani eski haline getirecek fonksiyon yazilir.
+            btnArkadas.Enabled = true; //butona tekrar tiklanmasi engellenir.
+            this.AutoSize = false; //form ekrani eski haline dondurulur.
+        }
+
+        private void EkranOlustur() //arkadas mi butonuna bastiktan sonraki ekrani olusturacak fonksiyondur.
+        {
+            //Olusturulacak araclarin konumunu yüksekligini genisligi atanir ve formun üzerine olusturulur.
+
+            lstBox.Location = new Point(330, 50);
+            lstBox.Width = 78;
+            lstBox.Height = 170;
+            this.Controls.Add(lstBox);
+
+
+            lstBox2.Location = new Point(420, 50);
+            lstBox2.Width = 78;
+            lstBox2.Height = 170;
+            this.Controls.Add(lstBox2);
+
+
+            lblX2.Location = new Point(358, 33);
+            lblX2.Width = 35;
+            lblX2.Height = 13;
+            lblX2.Text = "X";
+            this.Controls.Add(lblX2);
+
+
+            lblY2.Location = new Point(455, 33);
+            lblY2.Width = 35;
+            lblY2.Height = 13;
+            lblY2.Text = "Y";
+            this.Controls.Add(lblY2);
+
+
+            lblToplamlar.Location = new Point(250, 223);
+            lblToplamlar.Width = 78;
+            lblToplamlar.Height = 16;
+            lblToplamlar.Text = "TOPLAMLAR";
+            this.Controls.Add(lblToplamlar);
+
+
+            txtToplamXinAlti.Location = new Point(330, 220);
+            txtToplamXinAlti.Width = 78;
+            txtToplamXinAlti.Height = 16;
+            this.Controls.Add(txtToplamXinAlti);
+
+            txtToplamYinAlti.Location = new Point(420, 220);
+            txtToplamYinAlti.Width = 78;
+            txtToplamYinAlti.Height = 16;
+            this.Controls.Add(txtToplamYinAlti);
+        }
+
+        private void ArkadasMı() //sayilari carpanlarina ayirip carpanlarini toplayan fonksiyondur.
+        {
+            provider.Clear(); //Ekran daha onceden uyari iconu var ise silinmesi saglandi.
+            provider.BlinkRate = 0; //Uyarı iconunun hızını ayarlamak icin yazildi.
+
+            if (txtX.Text == "" || txtY.Text == "") //textboxların herhangi birisine sayı girilip girilmedigini kontrol eden kosuldur.
+            {
+                if (txtX.Text == "" && txtY.Text == "") //testboxların ikisine de sayi girilmemisse bu kosula girer ve uyarı verir.
+                {
+                    MessageBox.Show("X ve Y alanı boş geçilemez.", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    provider.SetError(txtX, "Bu alan boş geçilemez"); //uyarı iconlarini cagiririz.
+                    provider.SetError(txtY, "Bu alan boş geçilemez"); //uyarı iconlarini cagiririz.
+                }
+                else if (txtX.Text == "") //textboxlardan x sayisi bossa bu kosula girer ve uyarı verir.
+                {
+                    MessageBox.Show("X alanı boş geçilemez.", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    provider.SetError(txtX, "Bu alan boş geçilemez"); //uyarı iconlarini cagiririz.
+                }
+                else //textboxlardan y sayisi bossa bu kosula girer ve uyarı verir.
+                {
+                    MessageBox.Show("Y alanı boş geçilemez.", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    provider.SetError(txtY, "Bu alan boş geçilemez"); //uyarı iconlarini cagiririz.
+                }
+                state = true;
+            }
+            else //sayi girilmisse buraya gelir.
+            {
+                sayi1 = int.Parse(txtX.Text);
+                
+                for (int i = 1; i < sayi1; i++) //x sayisini carpanlarina ayirmak icin kullanilan dongudur.
+                {
+                    if (sayi1 % i == 0) //x sayisini tam bolup bolmedigini sinayan kosuldur
+                    {
+                        lstBox.Items.Add(i); //tam bolerse listboxa ekler
+                        toplamX += i; //tam bolen sayilarin tolamini bulur.
+                    }
+                }
+                txtToplamXinAlti.Text = toplamX.ToString(); //tam bolen sayilarin toplamini textboxa yazdirir.
+
+                sayi2 = int.Parse(txtY.Text);
+
+                for (int i = 1; i < sayi2; i++)  //y sayisini carpanlarina ayirmak icin kullanilan dongudur.
+                {
+                    if (sayi2 % i == 0)  //y sayisini tam bolup bolmedigini sinayan kosuldur
+                    {
+                        lstBox2.Items.Add(i);//tam bolerse listboxa ekler
+                        toplamY += i; //tam bolen sayilarin tolamini bulur.
+                    }
+                }
+                txtToplamYinAlti.Text = toplamY.ToString(); //tam bolen sayilarin toplamini textboxa yazdirir.
+
+                state = false; //arkadas mı olup olmadigini btnArkadas_click de kontrol etmek icin deger veriyoruz.
+                btnArkadas.Enabled = false; // hesaplandiktan sonra tekrar arkadas mi butonuna basilmasini engelledik
+            }
+        }
+
+        private void Temizle()
+        {
+            //EkranıYazdir fonksiyonu ile olusturulan araclari temizler.
+            this.Controls.Remove(lstBox);
+            this.Controls.Remove(lstBox2);
+            this.Controls.Remove(lblX2);
+            this.Controls.Remove(lblY2);
+            this.Controls.Remove(lblToplamlar);
+            this.Controls.Remove(txtToplamXinAlti);
+            this.Controls.Remove(txtToplamYinAlti);
+
+            foreach (Control item in this.Controls)  //textboxlarin icini temizleyen dongudur.
+            {
+                if (item is TextBox) // itemin textbox oldugunu sorgulayan kosuldur.
+                {
+                    TextBox tbox = (TextBox)item;
+                    tbox.Clear();
+                }
+            }
+            //listboxlarin ici temizlenir.İlk degerler sifirlanir.
+            lstBox.Items.Clear();
+            lstBox2.Items.Clear();
+            txtToplamXinAlti.Clear();
+            txtToplamYinAlti.Clear();
+            toplamX = 0; 
+            toplamY = 0;
+            provider.Clear();
+        }
+
+        private void txtX_KeyPress(object sender, KeyPressEventArgs e) //X Textbox'ına sadece sayı girisi kontrolu yapildi.
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void txtY_KeyPress(object sender, KeyPressEventArgs e) //Y Textbox'ına sadece sayı girisi kontrolu yapildi.
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void txtX_Validated(object sender, EventArgs e) //X textbox'i bos gecildiginde uyari verilmesi saglar.
+        {
+            provider.BlinkRate = 0;
+            if (txtX.Text.Trim() == "") // txtX boş olup olmadigini kontrol ettiren kosuldur.
+                provider.SetError(txtX, "X değerini girmelisiniz"); // ErrorProvider açılacak ve mesaj cikaracak
+            else
+                provider.SetError(txtX, "");  // ErrorProvider kapanacak
+
+        }
+
+        private void txtY_Validated(object sender, EventArgs e) //Y textbox'i bos gecildiginde uyari verilmesi saglar.
+        {
+            provider.BlinkRate = 0;
+            if (txtY.Text.Trim() == "") // txtY boş olup olmadigini kontrol ettiren kosuldur.
+                provider.SetError(txtY, "Y değerini girmelisiniz"); // ErrorProvider açılacak ve mesaj cikaracak.
+            else
+                provider.SetError(txtY, ""); // ErrorProvider kapanacak.
+        }
+    }
+}
